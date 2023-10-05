b4487bced8d328aab1a1ebe0abb255c845b229df
jxanip <juanikbalpratama@gmail.com>
2023-10-05 14:40:53 +0700

    fetch

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -2,7 +2,25 @@ import Navbars from "../../component/navbar";
 import Footer from "../Footer/Footer";
 import Cards from "../cards/card";
 import Banner from "../Banner/banner";
+import { useEffect, useState } from "react";
 
+function home = 
+const [laptop,setLaptop]= useState ([]);
+useEffect(()=>{
+  const fetchData =async () => {
+    try {
+      const response = await fetch ()
+      const jsonData = await response.json ();
+      const laptops = jsonData.laptop;
+      setLaptop(laptops);
+    
+    }  catch (error){
+      console.error("Error fetching data:",error)
+    } 
+  }
+  fetchData();
+},[]);
+console.log(laptop);
 const Home = () => {
   return (
     <div>
@@ -16,7 +34,6 @@ const Home = () => {
         <Cards />
         <Cards />
         <Cards />
-        
       </div> */}
       <Footer />
     </div>
